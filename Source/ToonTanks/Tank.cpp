6607799de30489c8645f96cab2bb3aa2d9be3cfc
ArchitTanwar <92805411+ArchitTanwar@users.noreply.github.com>
2023-03-13 19:17:21 +0530

    Add files via upload

---
+++ Source/ToonTanks/Tank.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
+
+ATank::ATank()
+{
+
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm Component"));
+    SpringArm->SetupAttachment(RootComponent);
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(SpringArm);
+
+
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+     Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+     PlayerInputComponent->BindAxis(TEXT("MoveForward"),this, &ATank::Move);
+     PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
+
+     PlayerInputComponent->BindAction(TEXT("Fire"), IE_Pressed, this, &ATank::Fire);
+     
+}
+
+void ATank::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if(TankPlayerController)
+    {
+        FHitResult HitResult;
+        TankPlayerController->GetHitResultUnderCursor(
+            ECollisionChannel::ECC_Visibility,
+            false,
+            HitResult
+        );
+       RotateTurret(HitResult.ImpactPoint);
+       
+}
+}
+
+void ATank::BeginPlay()
+{
+    Super::BeginPlay();
+
+    TankPlayerController = Cast<APlayerController>(GetController());
+
+    
+}
+
+void ATank::Move(float Value)
+{
+   FVector DeltaLocation(0.f);
+   //X = Value*DeltaTime*Speed
+   DeltaLocation.X = Value*UGameplayStatics::GetWorldDeltaSeconds(this)*Speed; 
+
+   AddActorLocalOffset(DeltaLocation, true);
+}
+
+void ATank::Turn(float Value)
+{
+    FRotator DeltaRotation(0.f);
+    DeltaRotation.Yaw = Value*TurnRate*UGameplayStatics::GetWorldDeltaSeconds(this);
+
+    AddActorLocalRotation(DeltaRotation, true);
+}
+
+void ATank::HandleDestruction()
+{
+    Super::HandleDestruction();
+    SetActorHiddenInGame(true);
+    SetActorTickEnabled(false);
+}
+
+
---
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -0,0 +1,53 @@
+#include "ToonTanksGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tank.h"
+#include "Tower.h"
+#include "ToonTanksPlayerController.h"
+ 
+void AToonTanksGameMode::ActorDied(AActor* DeadActor)
+{
+   if (DeadActor == Tank)
+   {
+       Tank->HandleDestruction();
+       if (ToonTanksPlayerController)
+       {
+           ToonTanksPlayerController->SetPlayerEnabledState(false);
+       }
+    }
+    else if (ATower* DestroyedTower = Cast<ATower>(DeadActor))
+    {
+        DestroyedTower->HandleDestruction();
+    }
+}
+ 
+void AToonTanksGameMode::BeginPlay() 
+{
+   Super::BeginPlay();
+   HandleGameStart();
+}
+ 
+void AToonTanksGameMode::HandleGameStart()
+{
+   Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+   ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
+
+   StartGame();
+   
+    if (ToonTanksPlayerController)
+    {
+        ToonTanksPlayerController->SetPlayerEnabledState(false);
+ 
+        FTimerHandle PlayerEnableTimerHandle;
+        FTimerDelegate PlayerEnableTimerDelegate = FTimerDelegate::CreateUObject(
+            ToonTanksPlayerController,
+            &AToonTanksPlayerController::SetPlayerEnabledState,
+            true
+        );
+        GetWorldTimerManager().SetTimer(
+            PlayerEnableTimerHandle,
+            PlayerEnableTimerDelegate,
+            StartDelay,
+            false
+        );
+    }
+}        
